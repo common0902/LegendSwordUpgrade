28d060fc9bfb951d6b5fe78df5b7d881da4187b0
chohangyeol09 <chohangyeol09@gmail.com>
2026-06-07 12:46:00 +0900

    update : chgasciiart

--- LegendSwordUpgrade/CHGData.h
+++ LegendSwordUpgrade/CHGData.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <map>
 
 struct CHGData
 {
+	std::map<string, int> haveTotem;
 
+	bool slotPlaying;
 };
--- LegendSwordUpgrade/SHOP.cpp
+++ LegendSwordUpgrade/SHOP.cpp
@@ -1,13 +1,22 @@
+#include "Console.h"
 #include "SHOP.h"
+#include "CHGAsciiArt.h"
+static AsciiObjs objs;
 
 void InitShop(GameState& state)
 {
+	AsciiInit(objs);
+	//state.ChgData.haveTotem[]
 }
 
 void UpdateShop(GameState& state)
 {
+	
 }
 
 void RenderShop(const GameState& state)
 {
+	AsciiRender(objs);
+
+
 }
