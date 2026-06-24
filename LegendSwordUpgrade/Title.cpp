2c8c70c1f47449cbca7797b8467d1f99e2df2883
common0902 <minsoung200@gmail.com>
2026-06-24 22:46:57 +0900

    title

--- LegendSwordUpgrade/Title.cpp
+++ LegendSwordUpgrade/Title.cpp
@@ -26,12 +26,26 @@ void TitleScene::Update()
 
 void TitleScene::Render() const
 {
-	GotoXY(0, 0);
-	cout << "Title";
+	GotoXY(WIDTH / 2 - 50, 5);
+		cout << "_______  _     _  _______  ______    ______     __   __  _______  _______  _______  _______  ______    "   ;   
+	GotoXY(WIDTH / 2 - 50, 6);
+		cout << "|       || | _ | ||       ||    _ |  |      |   |  |_|  ||   _   ||       ||       ||       ||    _ |  ";
+	GotoXY(WIDTH / 2 - 50, 7);
+		cout << "|  _____|| || || ||   _   ||   | ||  |  _    |  |       ||  |_|  ||  _____||_     _||    ___||   | ||  ";
+	GotoXY(WIDTH / 2 - 50, 8);
+		cout << "| |_____ |       ||  | |  ||   |_||_ | | |   |  |       ||       || |_____   |   |  |   |___ |   |_||_ ";
+	GotoXY(WIDTH / 2 - 50, 9);
+		cout << "|_____  ||       ||  |_|  ||    __  || |_|   |  |       ||       ||_____  |  |   |  |    ___||    __  |";
+	GotoXY(WIDTH / 2 - 50, 10);
+		cout << " _____| ||   _   ||       ||   |  | ||       |  | ||_|| ||   _   | _____| |  |   |  |   |___ |   |  | |";
+	GotoXY(WIDTH / 2 - 50, 11);
+		cout << "|_______||__| |__||_______||___|  |_||______|   |_|   |_||__| |__||_______|  |___|  |_______||___|  |_|";
+	GotoXY(WIDTH / 2 - 50, 12);
+
 	const string lables[] = { "SHOP", "UPGRADE", "BATTLE" };
 	for (int i = 0;i < 3;++i)
 	{
-		GotoXY(5, 5 + i);
+		GotoXY(WIDTH / 2 - 5, 30 + i);
 		cout << (i + 1 == state.LrwData.curMenu ? "> " : "  ") << lables[i];
 	}
 }
